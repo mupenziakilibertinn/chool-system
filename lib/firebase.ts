07b79308925a45f190ed73997730b549d72314fb
Mupenzi Akili Bertin <mupenziakilibertinn@gmail.com>
2026-05-20 15:13:58 +0200

    linked authentic configuration keys for school-system-e05c0

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyYourActualSecretKeyFromFirebaseConsole", 
-  authDomain: "chool-system-bnmq.firebaseapp.com",
-  projectId: "chool-system-bnmq",
-  storageBucket: "chool-system-bnmq.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID_NUMBERS",
-  appId: "YOUR_APP_ID_STRING"
+  apiKey: "AIzaSyDqaBEM57Q_k_sGfv2A7RWifxJ0EhnczeE",
+  authDomain: "school-system-e05c0.firebaseapp.com",
+  projectId: "school-system-e05c0",
+  storageBucket: "school-system-e05c0.firebasestorage.app",
+  messagingSenderId: "69366841525",
+  appId: "1:69366841525:web:03ea99c608542f81777efc"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
